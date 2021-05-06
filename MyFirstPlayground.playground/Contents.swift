70fe624556aab7ace70d757682d2c1d5a7f967a6
AppleLab <applelab@iMac-AppleLab-6.local>
2021-05-05 23:00:04 -0700

    add .gitignore and fst playground

---
+++ MyFirstPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+
+var str = "Hello, playground"
+var integer = 123
+var decimal = 12.34
+
+let PATH: String = "/home/user/desktop/files"
+let NUMBER: Int = 100
+let PI: Double = 3.141592
+
+let literal1 = str + " " + String(integer)
+let literal2 = "\(str) \(decimal)"
+print(literal1)
+print(literal2)
