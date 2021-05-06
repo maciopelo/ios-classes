5f23b25e376c889f027f7a33d9ef8c524cc0a339
AppleLab <applelab@iMac-AppleLab-6.local>
2021-05-05 23:08:04 -0700

    create fst weather app project

---
+++ MyFirstWeatherApp/MyFirstWeatherApp/MyFirstWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstWeatherAppApp.swift
+//  MyFirstWeatherApp
+//
+//  Created by AppleLab on 05/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
